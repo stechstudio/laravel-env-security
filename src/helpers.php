0b722f9653b4007111caa24e847cb5ee6753b70d
Bubba <rob@stechstudio.com>
2018-06-08 19:15:03 -0400

    Helpers

---
+++ src/helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+if (! function_exists('env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param string $key
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    function env($key, $default = null)
+    {
+        $value = getenv($key);
+        if (false === $value) {
+            return value($default);
+        }
+        switch (strtolower($value)) {
+            case 'true':
+            case '(true)':
+                return true;
+            case 'false':
+            case '(false)':
+                return false;
+            case 'empty':
+            case '(empty)':
+                return '';
+            case 'null':
+            case '(null)':
+                return;
+        }
+        if (($valueLength = strlen($value)) > 1 && '"' === $value[0] && '"' === $value[$valueLength - 1]) {
+            return substr($value, 1, -1);
+        }
+
+        return $value;
+    }
+}
