68883eb1b1ec9234ec9512ecdf458ef4d4768ac2
Bubba <rob@stechstudio.com>
2018-06-12 17:02:01 -0400

    A Facade

---
+++ src/Facades/KMSDotEnv.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace STS\Kms\DotEnv\Facades;
+
+use Illuminate\Support\Facades\Facade;
+use STS\Kms\DotEnv\Crypto\Client;
+
+/**
+ * Class Profile.
+ *
+ *
+ * @method static Client factory(string $keyId, array $config = [])
+ * @method static Client setKeyId(string $keyId)
+ * @method static Client encrypt($plaintext, array $options = [])
+ * @method static Client decrypt($cyphertextBlob, array $options = [])
+ * @method static Client decryptFile(string $path, array $options = [])
+ * @method static string ciphertextBase64()
+ * @method static string|resource|StreamInterface blob()
+ * @method static Client encryptFile(string $path, array $options = [])
+ * @method static Client saveEncryptedFile(string $path)
+ * @method static Client saveDecryptedFile(string $path)
+ * @method static string plaintext()
+ */
+class KMSDotEnv extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'sts.kmsdotenv';
+    }
+}
