78e6deb2cd0aa6cfddd342cd167ec7117b1c030a
Bubba <rob@stechstudio.com>
2018-06-12 17:02:37 -0400

    Finally, a configuration file.

--- config/kms.php
+++ config/kms.php
@@ -1,4 +1,9 @@
 <?php
 
 return [
+    'kms_key_id' => env('AWS_KMS_KEY'),
+    'kms_key_region' => env('AWS_REGION', 'us-east-1'),
+    'file_plaintext' => base_path('.env'),
+    'file_ciphertext' => sprintf('%s.env.enc', env('APP_ENV')),
+    'dir_ciphertext' => base_path('dotenv'),
 ];
