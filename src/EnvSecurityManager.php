281a81356d7796397c1405b01965cd8b8ef25c44
Joseph <joseph@stechstudio.com>
2021-02-02 15:50:42 -0500

    just use the helper method, eliminate the app vs container issue

--- src/EnvSecurityManager.php
+++ src/EnvSecurityManager.php
@@ -88,7 +88,7 @@ class EnvSecurityManager extends Manager
      */
     public function getDefaultDriver()
     {
-        return $this->container['config']['env-security.default'];
+        return config('env-security.default');
     }
 
     /**
@@ -96,7 +96,7 @@ class EnvSecurityManager extends Manager
      */
     public function createKmsDriver()
     {
-        $config = $this->container['config']['env-security.drivers.kms'];
+        $config = config('env-security.drivers.kms');
 
         $key = $this->keyResolver
             ? $this->resolveKey()
@@ -110,7 +110,7 @@ class EnvSecurityManager extends Manager
      */
     public function createGoogleKmsDriver()
     {
-        $config = $this->container['config']['env-security.drivers.google_kms'];
+        $config = config('env-security.drivers.google_kms');
 
         if ($this->keyResolver) {
             $config['key_id'] = $this->resolveKey();
